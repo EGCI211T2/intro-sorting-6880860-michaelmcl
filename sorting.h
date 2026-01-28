885a0f7da28ab8a94d824bbe6c19b9006547410c
6880860-michaelmcl <michael.mcl@student.mahidol.edu>
2026-01-28 08:54:52 +0000

    my second commit

--- sorting.h
+++ sorting.h
@@ -1,6 +1,7 @@
 void swap(int &a,int &b);
 void sort(int *a,int N);
 void display(int *a,int N);
+void selectionsort(int *a, int N);
 using namespace std;
 
 
@@ -35,7 +36,26 @@ void display(int a[], int N)
     int i;
     for(i=0;i<N;i++)
     {
-        cout<<a[i]<<" "<<endl;
+        cout<<a[i]<<" ";
     }
+    cout<<endl;
 
+}
+
+void selectionsort(int *a, int N)
+{
+    int j;
+    for(j=0; j<N-1; j++)
+    {
+        int i,mi=j, min=a[j];
+    for (i=j;i<N;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+            mi=i;
+        }
+    } 
+    swap (a[j], a[mi]);
+    }
 }
